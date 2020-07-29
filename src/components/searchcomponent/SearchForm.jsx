c490491520abdfd08e796de6ad1264eafd9569f3
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-07-29 16:25:41 -0700

    adds search form with onChange and onSubmit working

---
+++ src/components/searchcomponent/SearchForm.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+//preventDefault in hook
+
+const SearchForm = ({ searchQuery, onChange, onSubmit }) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" name="search-query" value={searchQuery} onChange={onChange} />
+      <button>Search Artist</button>
+    </form>
+  );
+};
+
+SearchForm.propTypes = {
+  searchQuery: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired
+};
+
+export default SearchForm;
--- src/components/searchcomponent/SearchPage.jsx
+++ src/components/searchcomponent/SearchPage.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
+import SearchForm from './SearchForm';
 
 const SearchPage = () => {
   return (
     <div>
       {/* input, button and pagination on this page */}
       {/* <Pagination totalOffset={totalOffset}/> */}
-      
+      <SearchForm />
     </div>
   );
 };
 
-
 export default SearchPage;
-
