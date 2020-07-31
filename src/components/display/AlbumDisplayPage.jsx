4e5bef5eda7192bce443113afa463e27a98c78a2
langstonBS <langston.beckwithstanley@gmail.com>
2020-07-31 11:54:53 -0700

    the beginging of the abumDisplay

--- src/components/display/AlbumDisplayPage.jsx
+++ src/components/display/AlbumDisplayPage.jsx
@@ -4,11 +4,10 @@ import {getRecordingsList} from '../../services/getArtists';
 export default class AlbumDisplayPage extends Component {
 
   
-  
   render() {
     return (
       <div>
-        
+        <hi>pleas workd you thi</hi>
       </div>
     )
   }
