04d7b68cf8675c289959c9adcb2d75d132b8af74
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-07-31 11:11:01 -0700

    created the fetch calls

--- src/services/getArtists.js
+++ src/services/getArtists.js
@@ -8,3 +8,47 @@ export const getArtists = (artistName, offset) => {
     .then(res => res.json()
     );
 };
+
+export const getOneArtist = (artistId) => {
+  return fetch(`http://musicbrainz.org/ws/2/release?artist=${artistId}&fmt=json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(res => res.json()
+    );
+};
+
+export const getCoverArt = (releaseId) => {
+  return fetch(`http://coverartarchive.org/release/${releaseId}front`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(res => res.json()
+    );
+};
+
+export const getRecordingsList = (releaseId) => {
+  return fetch(`http://musicbrainz.org/ws/2/recording?release=${releaseId}&fmt=json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(res => res.json()
+    );
+};
+
+export const getLyrics = (artistName, songTitle) => {
+  return fetch(`https://api.lyrics.ovh/v1/${artistName}/${songTitle}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(res => res.json()
+    );
+};
